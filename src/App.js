9ade79540ae18a573905336c7b5a433070699b41
abhin_2122003 <abhinshelly@gmail.com>
2023-05-21 13:24:06 +0530

    pppp

--- src/App.js
+++ src/App.js
@@ -8,15 +8,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          R-test
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
+          Published
         </a>
       </header>
     </div>
