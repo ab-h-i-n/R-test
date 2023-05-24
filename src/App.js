fa4c21419a6ffaef031a106e4da88dbb1f4e4609
abhin_2122003 <abhinshelly@gmail.com>
2023-05-24 11:43:54 +0530

    nav

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./navbar";
+import React from "react";
+import "./App.css"
+import {Route , Routes} from "react-router-dom"
+import Home from "./pages/home";
+import Pricing from "./pages/pricing";
+import About from "./pages/about";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          R-test
-        </p>
-        <a
-          className="App-link"
-        >
-          Published
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+  return <>
+  <Navbar/>
+  <div className="container">
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/Pricing" element={<Pricing/>}/>
+      <Route path="/About" element={<About/>}/>
+    </Routes>
+    </div></>
+          
 }
 
 export default App;
